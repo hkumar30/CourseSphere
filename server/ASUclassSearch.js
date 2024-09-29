f986ff656d7dc2c263c2a3bb900cd34196c007b2
Amal Shifwath Shaik <ashaik22@asu.edu>
2024-09-29 05:38:36 -0700

    Added express support to scraper

--- server/ASUclassSearch.js
+++ server/ASUclassSearch.js
@@ -26,42 +26,49 @@ async function asuClassSearch(subject, courseNumber) {
     
     await page1.waitForSelector('#class-results');
     
-    const elementInfo = await page1.evaluate(() => {
-        function getElementInfo(element) {
-            // const getStyles = (elem) => {
-            //     const computedStyles = window.getComputedStyle(elem);
-            //     let styles = {};
-            //     for (let i = 0; i < computedStyles.length; i++) {
-            //         const prop = computedStyles[i];
-            //         styles[prop] = computedStyles.getPropertyValue(prop);
-            //     }
-            //     return styles;
-            // };
+    // const elementInfo = await page1.evaluate(() => {
+    //     function getElementInfo(element) {
+    //         // const getStyles = (elem) => {
+    //         //     const computedStyles = window.getComputedStyle(elem);
+    //         //     let styles = {};
+    //         //     for (let i = 0; i < computedStyles.length; i++) {
+    //         //         const prop = computedStyles[i];
+    //         //         styles[prop] = computedStyles.getPropertyValue(prop);
+    //         //     }
+    //         //     return styles;
+    //         // };
         
-            const getElementData = (elem) => {
-                return {
-                    tagName: elem.tagName.toLowerCase(),
-                    id: elem.id,
-                    classes: Array.from(elem.classList),
-                    // styles: getStyles(elem),
-                    innerHTML: elem.innerHTML,
-                    children: Array.from(elem.children).map(getElementData)
-                };
-            };
+    //         const getElementData = (elem) => {
+    //             return {
+    //                 tagName: elem.tagName.toLowerCase(),
+    //                 id: elem.id,
+    //                 classes: Array.from(elem.classList),
+    //                 // styles: getStyles(elem),
+    //                 innerHTML: elem.innerHTML,
+    //                 children: Array.from(elem.children).map(getElementData)
+    //             };
+    //         };
         
-            return getElementData(element);
-        }
+    //         return getElementData(element);
+    //     }
 
-        const targetElement = document.querySelector('#class-results');
-        return getElementInfo(targetElement);
-    });
+    //     const targetElement = document.querySelector('#class-results');
+    //     return getElementInfo(targetElement);
+    // });
 
-    console.log(JSON.parse(elementInfo, null, 2));
+    // console.log(JSON.parse(elementInfo, null, 2));
 
-    await page1.screenshot({ path: '/Users/amalshifwathshaik/Desktop/Sunhacks2024/CourseSphere/server/screenshot.png', fullPage: true });
-    await browser.close();
+    // await page1.screenshot({ path: '/Users/amalshifwathshaik/Desktop/Sunhacks2024/CourseSphere/server/screenshot.png', fullPage: true });
     
-    return elementInfo;
+    
+    // return elementInfo;
+    const element = await page1.evaluate(() => {
+        const element = document.querySelector('#class-results');  // Select the element by id
+        return element ? element.outerHTML : null;  // Get the entire HTML of the element with its children
+      });
+    // const element = document.querySelector('div#class-results');  // For example, a div with class container
+    await browser.close();
+    return element ? element.outerHTML : null;  // Get the entire HTML of the element
 }
 
-asuClassSearch('CSE', '101');
+module.exports = asuClassSearch;
